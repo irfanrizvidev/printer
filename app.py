d1f88e9e445d4f52aceabbb66c7dddbb5fb4f99c
irfanrizvidev <irfanrizvidev@gmail.com>
2020-03-21 19:45:11 +0000

    flask mongodb connection and test

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
+
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+mongo = PyMongo(app)
 
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+@app.route('/get_users')
+def get_users():
+    return render_template("users.html", users=mongo.db.users.find())
 
 
 if __name__ == '__main__':
