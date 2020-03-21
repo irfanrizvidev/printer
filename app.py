c3a07363ea61ed533933ad3c4970df3b64443064
irfanrizvidev <irfanrizvidev@gmail.com>
2020-03-21 22:38:26 +0000

    Login template and logic implemented

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, session
 from flask_pymongo import PyMongo
-from bson.objectid import ObjectId 
+from bson.objectid import ObjectId
+import bcrypt
 
 
 app = Flask(__name__)
@@ -10,12 +11,35 @@ mongo = PyMongo(app)
 
 
 @app.route('/')
-@app.route('/get_users')
-def get_users():
+def index():
+    if 'username' in session:
+        return 'You are logged in as ' + session['username']
+        
     return render_template("users.html", users=mongo.db.users.find())
+    
+
+@app.route('/login', methods=['POST'])
+def login():
+    users = mongo.db.users
+    login_user = users.find_one({'username' : request.form['username']})
+
+    if login_user:
+        if bcrypt.hashpw(request.form['password'].encode('utf-8'), login_user['password'].encode('utf-8')) == login_user['password'].encode('utf-8'):
+            session['username'] = request.form['username']
+            return redirect(url_for('index'))
+
+    return 'Invalid username/password combination'
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
+    app.secret_key = os.getenv('PRINTER_FLASK_KEY')
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
             debug=True)
+    
